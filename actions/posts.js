50204f3dab1df8632aa5cdf7f1cf85e9938a543e
Deepu Cyriac <deepucvk123@gmail.com>
2024-07-31 14:19:36 +0530

    Added some client side validation (needs fixing) and put server actions into separate files.

---
+++ actions/posts.js
@@ -0,0 +1,44 @@
+"use server";
+//check for server-only npm package in next.js docs
+//it can be used for better security & ensure the code only runs on server
+import { redirect } from "next/navigation";
+import { storePost } from "@/lib/posts";
+
+//this is used to define server actions inside its own file
+//server actions can be organized to a folder in actions folder(any name)
+//multiple server actions can be added to this file itself
+
+export async function createPost(prevState, formData) {
+  //the below is added if server actions is defined inside a component file
+  //"use server"; //add this to convert a function into server action - a function which executes on server
+  //server actions should also be async functions
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+  await storePost({
+    imageUrl: "",
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+}
--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,50 +1,7 @@
-import FormSubmit from "@/components/form-submit";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+import PostForm from "@/components/post-form";
+
+import { createPost } from "@/actions/posts";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server"; //add this to convert a function into server action - a function which executes on server
-    //server actions should also be async functions
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
